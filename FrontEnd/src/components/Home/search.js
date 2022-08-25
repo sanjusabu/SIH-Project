f46261d6f8be0a43062c6d9265c2b6e0663aad42
Meet Jain <93723420+MeetJain20@users.noreply.github.com>
2022-08-25 12:34:48 +0530

    SPECIALITES COMPONENT

--- FrontEnd/src/components/Home/search.js
+++ FrontEnd/src/components/Home/search.js
@@ -4,8 +4,8 @@ import { useState } from "react";
 import Joblist from "./Joblist";
 import LoadingSpinner from "../../Design/UIElements/LoadingSpinner";
 import StateList from "./countries-states-cities";
-import classes from "./Home.module.css";
 import Topjobs from "./Topjobs";
+import Specialities from "./Specialities";
 
 const isSearch = (value) => value.trim() !== "";
 const Search = () => {
@@ -60,127 +60,9 @@ const Search = () => {
       <div>{Loading && <LoadingSpinner />}</div>
 
       {/* <StateList /> */}
-      {/* <div>
-
-         <Joblist details={details}/>
-
-        </div> */}
-      <div className={classes.main}>
-        <div className={classes.he}>
-          <h3>Job search according to the location</h3>
-        </div>
-        {/* <div className={classes.row}> */}
-        <div className={classes.im}>
-          <img
-            src={require("./jobslocation.jpg").default}
-            height="180"
-            width="180"
-          />
-        </div>
-        <div className={classes.para}>
-          <h6>
-            Services to help you get hired, faster: from preparing your CV,
-            getting recruiter attention, finding the right jobs, and more!
-          </h6>
-        </div>
-      </div>
-      <div className={classes.main}>
-        <div className={classes.he}>
-          <h3>Recommending skills for a job</h3>
-        </div>
-        {/* <div className={classes.row}> */}
-        <div className={classes.im}>
-          <img
-            src={require("./recommend.png").default}
-            height="180"
-            width="180"
-          />
-        </div>
-        <div className={classes.para}>
-          <h6>
-            Services to help you get hired, faster: from preparing your CV,
-            getting recruiter attention, finding the right jobs, and more!
-          </h6>
-        </div>
-      </div>
-      <div className={classes.main}>
-        <div className={classes.he}>
-          <h3>Keep a track of your career</h3>
-        </div>
-        {/* <div className={classes.row}> */}
-        <div className={classes.im}>
-          <img src={require("./keep.png").default} height="180" width="180" />
-        </div>
-        <div className={classes.para}>
-          <h6>
-            Services to help you get hired, faster: from preparing your CV,
-            getting recruiter attention, finding the right jobs, and more!
-          </h6>
-        </div>
-      </div>
-
-      {/* </div> */}
-      <Joblist details={details} />
-      <br></br>
-      <h3>Explore Top Companies Hiring Now</h3>
-      {/* <div className={classes.com}>
-        <div className={classes.main1}>
-
-          <br></br>
-          <br></br>
-        <h4>Apisero</h4>
-        <h6>Rating: 4.2</h6>
-        <h6>Reviews: 113</h6>
-        </div>
-        
-       
-
-         <div className={classes.c1}>
-        <div className={classes.main2}>
-        <br></br>
-          <br></br>
-          <br></br>
-
-        <h4>Coforge</h4>
-        <h6>Rating: 3.8</h6>
-        <h6>Reviews: 200</h6>
-        </div>
-        </div>
-      
-      <div className={classes.c2}>
-        <div className={classes.main3}>
-        <br></br>
-          <br></br>
-          <br></br>
-        <h4>Diebold</h4>
-        <h6>Rating: 4.0</h6>
-        <h6>Reviews: 150</h6>
-        
-        </div>
-        </div>
-        </div> */}
-
-      <div className="container">
-        <div className="row">
-          {/* <div className="row"> */}
-          <div className={["col-md-4", classes.main1].join(" ")}>
-            <h4>Apisero</h4>
-            <h6>Rating: 4.2</h6>
-            <h6>Reviews: 113</h6>
-          </div>
-          <div className={["col-md-4", classes.main1].join(" ")}>
-            <h4>Apisero</h4>
-            <h6>Rating: 4.2</h6>
-            <h6>Reviews: 113</h6>
-          </div>
-          <div className={["col-md-4", classes.main1].join(" ")}>
-            <h4>Apisero</h4>
-            <h6>Rating: 4.2</h6>
-            <h6>Reviews: 113</h6>
-          </div>
-        </div>
-      </div>
+      <Specialities />
       <Topjobs />
+      <Joblist details={details} />
     </>
   );
 };
