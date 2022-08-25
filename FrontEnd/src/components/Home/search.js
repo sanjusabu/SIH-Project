2639bdab872838ffc7a87a80cefd61fe038341da
Meet Jain <93723420+MeetJain20@users.noreply.github.com>
2022-08-25 11:54:53 +0530

    navbar

--- FrontEnd/src/components/Home/search.js
+++ FrontEnd/src/components/Home/search.js
@@ -59,7 +59,7 @@ const Search = () => {
       </div>
       <div>{Loading && <LoadingSpinner />}</div>
 
-      <StateList />
+      {/* <StateList /> */}
       {/* <div>
 
          <Joblist details={details}/>
--- FrontEnd/src/components/NavBar/NavBar.js
+++ FrontEnd/src/components/NavBar/NavBar.js
@@ -17,7 +17,7 @@ const NavBar = () => {
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
           <Link className="navbar-brand" to="#">
-            Navbar
+            Ez Career
           </Link>
           <button
             className="navbar-toggler"
