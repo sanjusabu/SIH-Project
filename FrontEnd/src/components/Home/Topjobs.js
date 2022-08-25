878d7d7779831bcca468781bb1f87138e60db72e
Meet Jain <93723420+MeetJain20@users.noreply.github.com>
2022-08-25 11:42:37 +0530

    homepage best jobs

---
+++ FrontEnd/src/components/Home/Topjobs.js
@@ -0,0 +1,98 @@
+import React from "react";
+import classes from "./Topjobs.module.css";
+
+const Topjobs = () => {
+  return (
+    <div className="container my-3">
+      <div className="jobs">
+        <h1 className={classes.headd}>Suggested Jobs</h1>
+        <div className={classes.optionss}>
+          <h3 className={classes.titlee}>Amazon</h3>
+          <h4 className={classes.servicee}>Software Developer</h4>
+          <h5 className={classes.timee}>80000</h5>
+          <div className={classes.containerss}>
+            <ul className={classes.proglanguagee}>
+              <li className={classes.listtt}>Skills:</li>
+              <li className={classes.listtt}>Python</li>
+              <li className={classes.listtt}>Javascript</li>
+            </ul>
+          </div>
+        </div>
+        <div className={classes.optionss}>
+          <h3 className={classes.titlee}>Amazon</h3>
+          <h4 className={classes.servicee}>Software Developer</h4>
+          <h5 className={classes.timee}>80000</h5>
+          <div className={classes.containerss}>
+            <ul className={classes.proglanguagee}>
+              <li className={classes.listtt}>Skills:</li>
+              <li className={classes.listtt}>Python</li>
+              <li className={classes.listtt}>Javascript</li>
+            </ul>
+          </div>
+        </div>
+        <div className={classes.optionss}>
+          <h3 className={classes.titlee}>Amazon</h3>
+          <h4 className={classes.servicee}>Software Developer</h4>
+          <h5 className={classes.timee}>80000</h5>
+          <div className={classes.containerss}>
+            <ul className={classes.proglanguagee}>
+              <li className={classes.listtt}>Skills:</li>
+              <li className={classes.listtt}>Python</li>
+              <li className={classes.listtt}>Javascript</li>
+            </ul>
+          </div>
+        </div>
+        <div className={classes.optionss}>
+          <h3 className={classes.titlee}>Amazon</h3>
+          <h4 className={classes.servicee}>Software Developer</h4>
+          <h5 className={classes.timee}>80000</h5>
+          <div className={classes.containerss}>
+            <ul className={classes.proglanguagee}>
+              <li className={classes.listtt}>Skills:</li>
+              <li className={classes.listtt}>Python</li>
+              <li className={classes.listtt}>Javascript</li>
+            </ul>
+          </div>
+        </div>
+        <div className={classes.optionss}>
+          <h3 className={classes.titlee}>Amazon</h3>
+          <h4 className={classes.servicee}>Software Developer</h4>
+          <h5 className={classes.timee}>80000</h5>
+          <div className={classes.containerss}>
+            <ul className={classes.proglanguagee}>
+              <li className={classes.listtt}>Skills:</li>
+              <li className={classes.listtt}>Python</li>
+              <li className={classes.listtt}>Javascript</li>
+            </ul>
+          </div>
+        </div>
+        <div className={classes.optionss}>
+          <h3 className={classes.titlee}>Amazon</h3>
+          <h4 className={classes.servicee}>Software Developer</h4>
+          <h5 className={classes.timee}>80000</h5>
+          <div className={classes.containerss}>
+            <ul className={classes.proglanguagee}>
+              <li className={classes.listtt}>Skills:</li>
+              <li className={classes.listtt}>Python</li>
+              <li className={classes.listtt}>Javascript</li>
+            </ul>
+          </div>
+        </div>
+        <div className={classes.optionss}>
+          <h3 className={classes.titlee}>Amazon</h3>
+          <h4 className={classes.servicee}>Software Developer</h4>
+          <h5 className={classes.timee}>80000</h5>
+          <div className={classes.containerss}>
+            <ul className={classes.proglanguagee}>
+              <li className={classes.listtt}>Skills:</li>
+              <li className={classes.listtt}>Python</li>
+              <li className={classes.listtt}>Javascript</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Topjobs;
